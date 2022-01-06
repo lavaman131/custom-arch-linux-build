0f276bf2e1e1bdcf33f3fe48222e51380c8693af
lavaman131 <lavaalex3@gmail.com>
2022-01-05 23:47:44 -0800

    added config files

--- qtile/scripts/autostart.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-lxsession &
-xsetroot -cursor_name left_ptr &
-picom --config ~/.config/picom/picom.conf --experimental-backends &
-nitrogen --set-scaled ~/wallpapers/$((1 + $RANDOM % $(ls -1 | wc -l))).jpg &
-volnoti --timeout 2 &
